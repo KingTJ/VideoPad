983a7fd8a51afaa778f0d9b62c3fee81692f8a3b
Tabias Clements <tabias.clements@gmail.com>
2017-12-07 00:03:11 -0500

    Some Changes

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-    module.exports = {mongoURI: 'mongodb://<tabias>:<tabias>@ds033186.mlab.com:33186/videopad-prod'}
+    module.exports = {mongoURI: 'mongodb://tabias:tabias@ds033186.mlab.com:33186/videopad-prod'}
 } else{
     module.exports = {mongoURI: 'mongodb://localhost/VideoPad-dev'}  
 }
